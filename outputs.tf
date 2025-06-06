3d3ed403432b99f3db751b6c95258faf012dce38
Sonali2329 <sonaliagale9823@gmail.com>
2025-06-07 01:38:38 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+output "instance_public_ip" {
+  value = aws_instance.techeazy_app.public_ip
+}
+
+output "instance_url" {
+  value = "http://${aws_instance.techeazy_app.public_ip}"
+}
+
+output "private_key_path" {
+  value       = local_file.private_key.filename
+  description = "Path to the private SSH key"
+}
