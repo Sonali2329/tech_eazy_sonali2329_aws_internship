6a3b666f14f1deea6b3a2f8ada58e366cf0004fb
Sonali2329 <sonaliagale9823@gmail.com>
2025-06-11 09:37:01 +0530

    Adding 2nd Assignment

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket to store logs."
+  type        = string
+}
+
 variable "instance_type" {
-  default = "t2.micro"
+  description = "EC2 instance type"
+  default     = "t2.micro"
 }
