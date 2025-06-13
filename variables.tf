01be0f6a6ed6d953e34b22bda2d9d2dc12a8f811
Sonali2329 <sonaliagale9823@gmail.com>
2025-06-13 19:11:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket to store logs."
-  type        = string
-}
-
 variable "instance_type" {
   description = "EC2 instance type"
   default     = "t2.micro"
