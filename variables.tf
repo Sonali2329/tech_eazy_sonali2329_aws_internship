4c69c7f335d563c806722d63e4ddd96b6a5c4d67
Sonali2329 <sonaliagale9823@gmail.com>
2025-06-13 09:40:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "instance_type" {
   description = "EC2 instance type"
   default     = "t2.micro"
 }
+
+variable "stage" {
+  description = "Deployment stage (e.g., dev, prod)"
+  type        = string
+}
